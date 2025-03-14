43580a619bc4dc63d61c7dc003d2563cc19d487f
Bloodbat <bloodbad@prodigy.net.mx>
2025-03-14 04:53:27 -0600

    Add: buildrack helper: status messages and pretty colors.

--- buildrack.fish
+++ buildrack.fish
@@ -1,3 +1,7 @@
 #!/usr/bin/env fish
 set -x VERSION 2.6.1 #<--- Set me to the appropriate version!
-make -j4 #<--- Use as many or as few threads as you can... or like.
+set_color brcyan; echo Rack version set to: $VERSION
+set_color bryellow; echo Building Rack...
+make -j4 #<--- Use as many or as few threads as you want, can, or like.
+set_color brgreen; echo Done!
+set_color normal;
