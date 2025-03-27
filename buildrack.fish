d0e6f60efdf4edc82f3bca616de470e7750a994a
Bloodbat <bloodbad@prodigy.net.mx>
2025-03-27 11:38:05 -0600

    Update: Rack 2.6.3.

--- buildrack.fish
+++ buildrack.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/env fish
-set -x VERSION 2.6.2 #<--- Set me to the appropriate version!
-set_color brcyan; echo Rack version set to: $VERSION
+set -x RACK_VERSION 2.6.3 #<--- Set me to the appropriate version!
+set_color brcyan; echo Rack version set to: $RACK_VERSION
 set_color bryellow; echo Building Rack...
 make -j4 #<--- Use as many or as few threads as you want, can, or like.
 set_color brgreen; echo Done!
