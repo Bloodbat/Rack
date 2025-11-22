ca7506b328ab15e3e86e1640e3c0e6b22120c79f
Bloodbat <5965348+Bloodbat@users.noreply.github.com>
2025-11-22 15:04:30 -0600

    Update: version 2.6.6.

--- buildrack.fish
+++ buildrack.fish
@@ -1,5 +1,5 @@
 #!/usr/bin/env fish
-set -x RACK_VERSION 2.6.3 #<--- Set me to the appropriate version!
+set -x RACK_VERSION 2.6.6 #<--- Set me to the appropriate version!
 set_color brcyan; echo Rack version set to: $RACK_VERSION
 set_color bryellow; echo Building Rack...
 make -j4 #<--- Use as many or as few threads as you want, can, or like.
